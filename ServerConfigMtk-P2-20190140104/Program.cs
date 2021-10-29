63d33ea12b8167fd248cd764559d366cb4ccb06f
Eka Nova Pramudya <78529450+pramek008@users.noreply.github.com>
2021-10-29 22:48:26 +0700

    Add project files.

---
+++ ServerConfigMtk-P2-20190140104/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+using ServiceMtk_P1_20190140104;
+
+namespace ServerConfigMtk_P2_20190140104
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost hostObj = null;
+            try
+            {
+                hostObj = new ServiceHost(typeof(Matematika));
+                hostObj.Open();
+                Console.WriteLine("Server telah Siappp...");
+                Console.ReadLine();
+                hostObj.Close();
+            }
+            catch(Exception e)
+            {
+                hostObj = null;
+                Console.WriteLine(e.Message);
+                Console.ReadLine();
+            }
+        }
+    }
+}
